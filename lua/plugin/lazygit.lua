e84ee69bc8913cc607b14ec127c28ed0a4766b3b
SmolKaede <smolkaede@proton.me>
2024-03-23 15:48:02 -0500

    Don't lazyload lazygit, more private presence

--- lua/plugin/lazygit.lua
+++ lua/plugin/lazygit.lua
@@ -2,6 +2,7 @@ return{
     "kdheepak/lazygit.nvim",
     cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"},
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
     config = function()
         vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
     end
