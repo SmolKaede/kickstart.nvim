5299aa96d586434e6b239ae497fac2a517e7cf7d
SmolKaede <smolkaede@proton.me>
2024-03-23 21:26:06 -0500

    Formatting

--- lua/options.lua
+++ lua/options.lua
@@ -8,19 +8,19 @@ vim.opt.shiftwidth = 4		--insert 4 spaces on a tab
 vim.opt.expandtab = true	--tabs are spaces
 
 -- UI --
-vim.opt.number = true		--absolute number
-vim.opt.relativenumber = true	--Numbers on the left side
+vim.opt.number = true		 --absolute number
+vim.opt.relativenumber = true--Numbers on the left side
 vim.opt.cursorline = true	--highlight cursor line underneath the cursor horizontal
 vim.opt.splitbelow = true	--New vert split bottom
 vim.opt.splitright = true	--New horizontal splits right
-vim.opt.termguicolors = true	--24bit rgb color
-vim.opt.showmode = false		--show mode
+vim.opt.termguicolors = true--24bit rgb color
+vim.opt.showmode = false	--show mode, set to false due to presence of lualine
 vim.opt.scrolloff = 10		--10 lines of scrolloff
 vim.opt.signcolumn = "yes"
 
 -- Search --
 vim.opt.incsearch = true	--search as characters are entered
-vim.opt.hlsearch = false		--highlight matches
+vim.opt.hlsearch = false	--highlight matches
 vim.opt.ignorecase = true	--ignore case in search
 vim.opt.smartcase = true	--case sensitive if uppercase is entered
 
